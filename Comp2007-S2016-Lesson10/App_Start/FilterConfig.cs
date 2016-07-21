53667072be50ab88a017d1c91f1706e5b2b2de93
averyirin <averyirin@gmail.com>
2016-07-21 09:24:15 -0400

    Add project files.

---
+++ Comp2007-S2016-Lesson10/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Comp2007_S2016_Lesson10 {
+    public class FilterConfig {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
