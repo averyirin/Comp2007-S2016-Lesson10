59160145841b287b5da84eb7bb7cb1cd17ef9864
averyirin <averyirin@gmail.com>
2016-07-25 14:27:12 -0400

    Added Artist Model class to project. Closes Issue #1

---
+++ Comp2007-S2016-Lesson10/Models/Artist.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Comp2007_S2016_Lesson10.Models {
+    public class Artist {
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+    }
+}
